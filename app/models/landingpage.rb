f63bda7bf1bee8992c261f2b35d0e715ecfda7f9
lestgabo <lestgabo@gmail.com>
2018-11-21 23:52:05 -0800

    credentials string interpolation, irregardless of environment

--- app/models/landingpage.rb
+++ app/models/landingpage.rb
@@ -5,8 +5,8 @@ class Visitor
   validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
   def subscribe
-   mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
-   list_id = Rails.application.secrets.mailchimp_list_id
+   mailchimp = Gibbon::Request.new(api_key: Rails.application.credentials[Rails.env.to_sym][:mailchimp_api_key])
+   list_id = Rails.application.credentials[Rails.env.to_sym][:mailchimp_list_id]
    result = mailchimp.lists(list_id).members.create(
      body: {
        email_address: self.email,
