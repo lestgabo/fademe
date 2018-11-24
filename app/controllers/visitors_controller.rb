72290fe8c144a1eca5c22ef96057bb715f38c053
lestgabo <lestgabo@gmail.com>
2018-11-23 20:15:41 -0800

    fix endpoint routing error

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,19 +2,23 @@ class VisitorsController < ApplicationController
   include ApplicationHelper
 
   def new
-  @visitor = Visitor.new
+    @visitor = Visitor.new
   end
+
   def create
     @visitor = Visitor.new(secure_params)
     if @visitor.valid?
       @visitor.subscribe
       flash[:notice] = "Signed up #{@visitor.email}."
       redirect_to root_path
-  else
+    else
       render :new
     end
   end
+
+
   private
+
   def secure_params
     params.require(:visitor).permit(:email)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :visitors, only: [:new, :create]
+  get '/visitors', to: redirect('/')
   root to: 'visitors#new'
 
 end
