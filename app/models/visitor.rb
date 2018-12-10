6c5a6b2b15925dee81036b831b8a53b1ce8653f5
lestgabo <lestgabo@gmail.com>
2018-12-10 10:42:24 -0800

    format visitor model

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -5,13 +5,14 @@ class Visitor
   validates_format_of :email, with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
 
   def subscribe
-   mailchimp = Gibbon::Request.new(api_key: Rails.application.credentials[Rails.env.to_sym][:mailchimp_api_key])
-   list_id = Rails.application.credentials[Rails.env.to_sym][:mailchimp_list_id]
-   result = mailchimp.lists(list_id).members.create(
-     body: {
-       email_address: self.email,
-       status: 'subscribed'
-     })
-   Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
+    mailchimp = Gibbon::Request.new(api_key: Rails.application.credentials[Rails.env.to_sym][:mailchimp_api_key], symbolize_keys: true)
+    list_id = Rails.application.credentials[Rails.env.to_sym][:mailchimp_list_id]
+
+    result = mailchimp.lists(list_id).members.create(
+      body: {
+        email_address: self.email,
+        status: 'subscribed'
+      })
+    Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
   end
 end
