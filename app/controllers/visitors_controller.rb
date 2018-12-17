dd1a84e54b522142b658f29810a2d86abc0258d8
lestgabo <lestgabo@gmail.com>
2018-12-17 02:09:30 -0800

    3

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,4 +1,6 @@
 class VisitorsController < ApplicationController
+  require 'digest'
+  
   include ApplicationHelper
 
   def new
@@ -12,54 +14,25 @@ class VisitorsController < ApplicationController
     if @visitor.valid?
       mailchimp = Gibbon::Request.new(api_key: Rails.application.credentials[Rails.env.to_sym][:mailchimp_api_key], symbolize_keys: true)
       list_id = Rails.application.credentials[Rails.env.to_sym][:mailchimp_list_id]
-
-      mailchimp.timeout = 30
-      gibbon.open_timeout = 30
-
-      # check for 2x subscribers (why you do this?) https://stackoverflow.com/questions/50194825/check-if-member-exists-using-mailchimp-3-0-api-and-gibbon-rails?rq=1
-
       member_id = Digest::MD5.hexdigest(input_email)
       
-
       begin
-        # member_status = mailchimp.lists(list_id).members(member_id).retrieve.body[:status]
-
         member_status = mailchimp.lists(list_id).members(member_id).retrieve.body[:status]
-
-        mailchimp.lists(list_id).members(member_id).update(body: {interests: {'interest_id': true}})
-
       rescue Exception => e
-        puts "FUCK MAN"
-        puts e
-        if (member_status == nil) || (member_status != "subscribed")
-          result = mailchimp.lists(list_id).members.create(
-            body: {
-              email_address: input_email,
-              status: 'subscribed'
-            })
-          Rails.logger.info("Subscribed #{input_email} to MailChimp") if result
-          flash[:notice] = "Thank you for signing up #{input_email}!"
-        elsif member_status == "subscribed"
-          flash[:notice] = "You are already subscribed!"
-        elsif member_status == "unsubscribed"
-          flash[:danger] = "You already unsubscribed!"
-        end
+        puts "Houston, we have a problem: #{e.message} - #{e.raw_body}"
+      end      
 
+      if member_status == "subscribed"
+        flash[:notice] = "You are already subscribed!"
+      else
+        mailchimp.lists(list_id).members(member_id).upsert(
+          body: {
+            email_address: input_email,
+            status: 'subscribed'
+          }
+        )
+        flash[:notice] = "Thank you for signing up #{input_email}!"
       end
-
-      # if (member_status == nil) || (member_status != "subscribed")
-      #   result = mailchimp.lists(list_id).members.create(
-      #     body: {
-      #       email_address: input_email,
-      #       status: 'subscribed'
-      #     })
-      #   Rails.logger.info("Subscribed #{input_email} to MailChimp") if result
-      #   flash[:notice] = "Thank you for signing up #{input_email}!"
-      # elsif member_status == "subscribed"
-      #   flash[:notice] = "You are already subscribed!"
-      # elsif member_status == "unsubscribed"
-      #   flash[:danger] = "You already unsubscribed!"
-      # end
       redirect_to root_path
     else
       render 'new'
@@ -74,3 +47,58 @@ class VisitorsController < ApplicationController
   end
 
 end
+
+
+# mailchimp.timeout = 30
+# gibbon.open_timeout = 30
+
+# check for 2x subscribers (why you do this?) https://stackoverflow.com/questions/50194825/check-if-member-exists-using-mailchimp-3-0-api-and-gibbon-rails?rq=1
+
+
+# begin
+#   gibbon.campaigns.create(body: body)
+# rescue Gibbon::MailChimpError => e
+#   puts "Houston, we have a problem: #{e.message} - #{e.raw_body}"
+# end
+
+
+ # begin
+      #   # member_status = mailchimp.lists(list_id).members(member_id).retrieve.body[:status]
+
+      #   member_status = mailchimp.lists(list_id).members(member_id).retrieve.body[:status]
+
+      #   mailchimp.lists(list_id).members(member_id).update(body: {interests: {'interest_id': true}})
+
+      # rescue Exception => e
+      #   puts "FUCK MAN"
+      #   puts e
+      #   if (member_status == nil) || (member_status != "subscribed")
+      #     result = mailchimp.lists(list_id).members.create(
+      #       body: {
+      #         email_address: input_email,
+      #         status: 'subscribed'
+      #       })
+      #     Rails.logger.info("Subscribed #{input_email} to MailChimp") if result
+      #     flash[:notice] = "Thank you for signing up #{input_email}!"
+      #   elsif member_status == "subscribed"
+      #     flash[:notice] = "You are already subscribed!"
+      #   elsif member_status == "unsubscribed"
+      #     flash[:danger] = "You already unsubscribed!"
+      #   end
+
+ 
+
+      # if (member_status == nil) || (member_status != "subscribed")
+      #   result = mailchimp.lists(list_id).members.create(
+      #     body: {
+      #       email_address: input_email,
+      #       status: 'subscribed'
+      #     })
+      #   Rails.logger.info("Subscribed #{input_email} to MailChimp") if result
+      #   flash[:notice] = "Thank you for signing up #{input_email}!"
+      # elsif member_status == "subscribed"
+      #   flash[:notice] = "You are already subscribed!"
+      # elsif member_status == "unsubscribed"
+      #   flash[:danger] = "You already unsubscribed!"
+      # end
+     
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # resources :visitors, only: [:new, :create]
-  resources :visitors
+  resources :visitors, only: [:new, :create]
   # get '/visitors', to: redirect('/')
   root to: 'visitors#new'
 
